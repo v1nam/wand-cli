995513ca584a5f26b42e518cffe6a08d75c86827
saiTama-max <vinamraj01@gmail.com>
2021-01-23 16:59:21 +0530

    Add base file class to commands_dict

--- wandbox/commands/__init__.py
+++ wandbox/commands/__init__.py
@@ -1,7 +1,9 @@
 from wandbox.commands.frominput import FromInput
 from wandbox.commands.fromfile import FromFile
+from wandbox.commands.base import Base
 
 commands_dict = {
     "fromfile": FromFile.runfile,
     "frominput": FromInput.askinp,
+    "base": Base.run,
 }
