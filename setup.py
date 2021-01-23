ad855057a8fb93c8e825a34e11ded242acfc5f92
saiTama-max <vinamraj01@gmail.com>
2021-01-23 13:44:13 +0530

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="wand-cli",
-    version="1.5",
+    version="1.6",
     author="vinam",
     author_email="vinamraj01@gmail.com",
     description="A cli tool to compile over 26 languages instantly using the wandbox api.",
