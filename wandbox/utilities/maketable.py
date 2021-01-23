3a3924afb113fecf90548a06187784248d392530
saiTama-max <vinamraj01@gmail.com>
2021-01-23 17:00:46 +0530

    Make the mktbl command general purpose

--- wandbox/utilities/maketable.py
+++ wandbox/utilities/maketable.py
@@ -1,29 +1,15 @@
+from pygments.styles import get_all_styles
 from rich.table import Table
 
 
 class MakeTable:
-    languages_table = [
-        ("python", "f#"),
-        ("c++", "scala"),
-        ("c", "swift"),
-        ("javascript", "typescript"),
-        ("java", "vim"),
-        ("haskell", "lua"),
-        ("bash", "nim"),
-        ("cmake", "php"),
-        ("elixir", "perl"),
-        ("d", "pony"),
-        ("sqlite", "go"),
-        ("lisp", "ruby"),
-    ]
-
-    def mktbl(self):
-        """Make the language table from rich library."""
+    def mktbl(self, cont):
+        """Make a table list from rich library."""
         l_table = Table(show_header=False)
         l_table.add_column(width=12, justify="center")
         l_table.add_column(width=12, justify="center")
 
-        for lang in self.languages_table:
+        for lang in cont:
             l_table.add_row(lang[0], lang[1])
         return l_table
 
